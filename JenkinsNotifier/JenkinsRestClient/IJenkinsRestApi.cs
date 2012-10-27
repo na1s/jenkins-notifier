96d27f29417979e1b24c21581567937acda3a2d2
na1s <nasyrov@gmail.com>
2012-10-28 00:45:34 +0500

    added base objects

---
+++ JenkinsNotifier/JenkinsRestClient/IJenkinsRestApi.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace JenkinsRestClient
+{
+    public interface IJenkinsRestApi
+    {
+        IEnumerable<Job> GetJobs();
+    }
+}
---
+++ JenkinsNotifier/JenkinsRestClient/Node.cs
@@ -0,0 +1,7 @@
+﻿namespace JenkinsRestClient
+{
+    public class Node
+    {
+         
+    }
+}
